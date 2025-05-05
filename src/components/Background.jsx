3fb91aa53587c0f813858129f44793138d252a53
RamirezBB <brbrayan1945@gmail.com>
2025-05-05 13:55:18 -0600

    Add mouse interaction prop to Balatro component in Background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,7 @@ import Balatro from './Balatro'
 function Background() {
   return (
     <div className='absolute h-full w-full z-[-1]'>
-      <Balatro pixelFilter={2000} />
+      <Balatro pixelFilter={2000} mouseInteraction={false} />
     </div>
   )
 }
