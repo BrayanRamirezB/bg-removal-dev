c4abaa548084e0594a8a13071e43a331426724fe
RamirezBB <brbrayan1945@gmail.com>
2025-05-05 14:39:59 -0600

    Add sample image handling with AnimatedImage component in ImageUpload

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -4,6 +4,7 @@ import 'filepond/dist/filepond.min.css'
 import { toast } from '@pheralb/toast'
 import TiltCard from './TiltCard'
 import Spinner from './Spinner'
+import AnimatedImage from './AnimatedImage'
 
 // Plugins
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
@@ -59,6 +60,28 @@ function ImageUpload() {
     }
   }
 
+  const handleAddSampleImage = async (imagePath) => {
+    try {
+      // Obtener la imagen desde la ruta proporcionada
+      const response = await fetch(imagePath)
+      const blob = await response.blob()
+
+      // Crear un objeto File simulando una subida
+      const file = new File([blob], 'sample-image.webp', {
+        type: 'image/webp'
+      })
+
+      setFiles([file])
+    } catch (error) {
+      toast.error({
+        text: 'Ha ocurrido un error al cargar la imagen de muestra ❌',
+        duration: 5000,
+        description: 'Por favor, intenta nuevamente.'
+      })
+      console.log('Error fetching sample image:', error)
+    }
+  }
+
   return (
     <div className='py-5 w-full min-h-[560px] flex flex-col items-center justify-center gap-y-2'>
       <FilePond
@@ -88,6 +111,49 @@ function ImageUpload() {
           </button>
         </TiltCard>
       )}
+
+      <section
+        className={`flex flex-col items-center justify-center gap-y-8 ${
+          files.length > 0 ? 'pt-10' : 'pt-25'
+        }`}
+      >
+        <h2 className='text-xl font-semibold'>
+          O intenta con una de estas imágenes de muestra:
+        </h2>
+        <div className='flex items-center justify-center gap-x-4'>
+          <AnimatedImage
+            src={'./samples/img1.webp'}
+            alt='Imagen de muestra 1'
+            size={150}
+            duration={300}
+            onClick={() => handleAddSampleImage('./samples/img1.webp')}
+          />
+
+          <AnimatedImage
+            src={'./samples/img2.webp'}
+            alt='Imagen de muestra 2'
+            size={150}
+            duration={300}
+            onClick={() => handleAddSampleImage('./samples/img2.webp')}
+          />
+
+          <AnimatedImage
+            src={'./samples/img3.webp'}
+            alt='Imagen de muestra 3'
+            size={150}
+            duration={300}
+            onClick={() => handleAddSampleImage('./samples/img3.webp')}
+          />
+
+          <AnimatedImage
+            src={'./samples/img4.webp'}
+            alt='Imagen de muestra 4'
+            size={150}
+            duration={300}
+            onClick={() => handleAddSampleImage('./samples/img4.webp')}
+          />
+        </div>
+      </section>
     </div>
   )
 }
