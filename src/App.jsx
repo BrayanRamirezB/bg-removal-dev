4636ecc5c377a7c3e459f0347f798e77a2b85a4e
RamirezBB <brbrayan1945@gmail.com>
2025-05-05 16:17:01 -0600

    Fix padding in header for improved layout consistency

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className='relative min-h-screen w-full'>
       <Background />
       <div className='flex flex-col items-center justify-center mx-auto max-w-5xl'>
-        <h1 className='text-4xl font-bold text-center pt-20 pb-5 flex items-center gap-x-4 text-neutral-50'>
+        <h1 className='text-4xl font-bold text-center pt-18 pb-5 flex items-center gap-x-4 text-neutral-50'>
           <span className='flex items-center justify-center'>
             <ImageIcon className='size-10' />
           </span>
